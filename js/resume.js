8cfdc755b0ae94fe153b2493563122c08305fc10
Shriyan Yamali <srujanshriyan@gmail.com>
2024-06-21 01:11:28 -0400

    Add files via upload

--- js/resume.js
+++ js/resume.js
@@ -7,3 +7,12 @@ document.querySelectorAll('.nav-link').forEach(anchor => {
         });
     });
 });
+
+document.addEventListener('scroll', function() {
+    const navbar = document.getElementById('mainNav');
+    if (window.scrollY > 0) {
+        navbar.style.backgroundColor = '#e3f0fd';
+    } else {
+        navbar.style.backgroundColor = '#dde9f9';
+    }
+});
