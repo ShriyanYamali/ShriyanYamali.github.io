bbcbedce74c7e38791934f04f2a16d93358baf93
Shriyan Yamali <srujanshriyan@gmail.com>
2024-06-24 21:58:57 -0400

    Add files via upload

--- mobile/js/scripts.js
+++ mobile/js/scripts.js
@@ -49,7 +49,39 @@ document.addEventListener("DOMContentLoaded", () => {
 
     let interval = null;
 
-    const h1Element = document.querySelector("h1");
+    const h1Element = document.querySelector("#sh");
+
+    // Your original code to run on page load
+    let iteration = 0;
+
+    clearInterval(interval);
+
+    interval = setInterval(() => {
+        h1Element.innerText = h1Element.innerText
+            .split("")
+            .map((letter, index) => {
+                if (index < iteration) {
+                    return h1Element.dataset.value[index];
+                }
+
+                return letters[Math.floor(Math.random() * 26)];
+            })
+            .join("");
+
+        if (iteration >= h1Element.dataset.value.length) {
+            clearInterval(interval);
+        }
+
+        iteration += 25 / 100;
+    }, 30);
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    let interval = null;
+
+    const h1Element = document.querySelector("#ya");
 
     // Your original code to run on page load
     let iteration = 0;
@@ -75,6 +107,7 @@ document.addEventListener("DOMContentLoaded", () => {
         iteration += 1 / 5;
     }, 30);
 });
+
 
 document.addEventListener("DOMContentLoaded", () => {
     const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
